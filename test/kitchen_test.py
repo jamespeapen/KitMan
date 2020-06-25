ce74b8f98366af39c2d62d8958f241ed0c913989
James Eapen <jamespeapen@gmail.com>
2020-06-25 15:54:40 -0400

    renames, kitchen test

--- test/kitchen_test.py
+++ test/kitchen_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../src')
-from src.kitchen import Food, Recipie
+from src.kitchen import Food, Recipie, Kitchen
 
 
 class TestKitchen:
@@ -8,18 +8,20 @@ class TestKitchen:
     def test_food(self):
 
         #test initializaiton 
-        beans = Food('beans', 'grains', 'bags')
-        assert beans.name == 'beans'
-        assert beans._unit == 'bags'
-        assert beans.quantity == 0
-        assert beans.quantity_needed == 1
-        assert beans.need()
+        food1 = Food('food1', 'grains', 'bags')
+        assert food1.name == 'food1'
+        assert food1._unit == 'bags'
+        assert food1.quantity == 0
+        assert food1.quantity_needed == 1
+
+        #test need food function
+        assert food1.need()
 
         #test setters 
-        beans.name = 'pinto'
-        assert beans.name == 'pinto'
-        beans.quantity = 5
-        assert not beans.need()
+        food1.name = 'food01'
+        assert food1.name == 'food01'
+        food1.quantity = 5
+        assert not food1.need()
 
     def test_recipie(self):
 
@@ -28,19 +30,25 @@ class TestKitchen:
         assert recipie1.name == 'test'
         assert recipie1.serving_number == 5
 
-        recipie1.name = 'test2'
-        assert recipie1.name == 'test2'
+        # test setters
+        recipie1.name = 'recipie1'
+        assert recipie1.name == 'recipie1'
         recipie1.serving_number = 10
         assert recipie1.serving_number == 10
 
         assert len(recipie1._ingredients) == 0
 
+        # add ingredient
         recipie1.add_ingredient('ingredient1', 5)
         assert recipie1._ingredients['ingredient1'] == 5
 
+        # duplciate ingredient
         assert recipie1.add_ingredient('ingredient1', 10) == 'Ingredient already present'
         assert recipie1._ingredients['ingredient1'] == 5
 
+    def test_kitchen(self):
 
-
+        # test init
+        kitchen = Kitchen()
+        assert  kitchen.recipie_count() == 0
 
