f38ce6d94baf86a02d68b4edb8cb8ba4224fe05c
James Eapen <jamespeapen@gmail.com>
2020-06-26 11:49:13 -0400

    file operations module

---
+++ src/data_operations.py
@@ -0,0 +1,44 @@
+'''
+Data operations for Kitman
+Read and write data to disk for persistent storage
+Created 26 June 2020
+Author: James Eapen
+'''
+
+from src.kitchen import Food, Recipie, Kitchen
+import json
+
+class Data:
+
+    def __init__(self):
+        self.writer
+        self.reader
+
+
+    def read_food(self):
+        #TODO: read all food from file
+
+
+    def write_food(self):
+        #TODO: write all food objects to file
+
+
+    def read_recipies(self):
+        #TODO: read all recipies from file
+
+
+    def write_recipies(self):
+        #TODO: write all recipies to file
+
+
+    def read_pantry(self):
+        #TODO: read pantry contents to memory
+
+    def write_pantry(self):
+        #TODO: write all pantry to file
+
+
+    def write_shopping_list(self):
+        #TODO: write shopping list to file
+
+
