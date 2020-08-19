9e03f70bd2a683405959522bd30781f281a20fb5
James Eapen <jamespeapen@gmail.com>
2020-08-18 23:00:27 -0400

    working menu with panels and data imports

--- src/menu.py
+++ src/menu.py
@@ -4,31 +4,73 @@ Created Aug 04 2020
 Author: James Eapen (jamespeapen@gmail.com)
 '''
 
-
+from data_operations import Data
+from kitchen import Kitchen
 import curses
-
+import pyfiglet
+import time
 
 class Menu:
 
     def __init__(self):
 
+        self.data = Data()
+        self.kitchen = Kitchen()
+
         self.stdscr = curses.initscr()
+        self.height, self.width = self.stdscr.getmaxyx()
+
+        # windows
+        self.menubar = curses.newwin(2, self.width, 0, 0)
+
+        self.data_window = curses.newwin(self.height-3,
+                                         2 * (self.width // 3), 3, 0)
+
+        self.preview_window = curses.newwin(self.height - 3,
+                                            self.width // 3, 4,
+                                            2 * (self.width // 3))
 
     def menu(self, stdscr, current_row):
 
-        height, width = self.stdscr.getmaxyx()
-
-        menu_bar = ["1: Kitchen", "2: Pantry", "3: Recipies", "4: Recipies", "5: Shopping"]
+        menu_bar = ["1: Pantry",
+                    "2: Recipies",
+                    "3: Shopping"]
 
         y = 0
         x = 1
 
         for idx, element in enumerate(menu_bar):
 
-            self.stdscr.addstr(y, x, element)
-            x += len(element) + len(element) //5
+            self.menubar.addstr(y, x, element)
+            x += len(element) + len(menu_bar) // (len(menu_bar) - 1)
 
-        self.stdscr.refresh()
+        self.menubar.refresh()
+
+    def pantry(self, stdscr):
+
+        self.data_window.clear()
+        self.data_window.addstr(0, 0, "Food\tQuantity in stock")
+        self.kitchen.pantry = self.data.read_pantry('test/test_read_pantry.json')
+        y = 2
+        x = 0
+
+        for food in self.kitchen.pantry:
+            self.data_window.addstr(y, x, food + '\t' + str(self.kitchen.pantry[food]))
+            y += 2
+        self.data_window.refresh()
+
+    def recipies(self, stdscr):
+
+        self.data_window.clear()
+        self.data_window.addstr(0, 0, "Recipies")
+        self.kitchen.recipies = self.data.read_recipies('test/test_read_recipies.json')
+        y = 2
+        x = 0
+
+        for recipie in self.kitchen.recipies:
+            self.data_window.addstr(y, x, recipie.name)
+            y += 2
+        self.data_window.refresh()
 
     def main(self, stdscr):
 
@@ -41,12 +83,18 @@ class Menu:
         while True:
             key = self.stdscr.getch()
 
-            if (key == curses.KEY_UP or key == ord("k")) and current_row > 0:
-                current_row -= 1
+            if key == ord('1'):
+                self.pantry(stdscr)
 
-            elif (key == curses.KEY_DOWN or key == ord("j")) \
-            and current_row < len(self.menu_list) - 1:
-                current_row += 1
+            elif key == ord('2'):
+                self.recipies(stdscr)
+#
+#            if (key == curses.KEY_UP or key == ord("k")) and current_row > 0:
+#                current_row -= 1
+#
+#            elif (key == curses.KEY_DOWN or key == ord("j")) \
+#            and current_row < len(self.menu_list) - 1:
+#                current_row += 1
 
             elif key == ord("q"):
                 break
@@ -56,4 +104,6 @@ class Menu:
 
 
 menu1 = Menu()
+print(pyfiglet.figlet_format("KitMan"))
+time.sleep(2)
 curses.wrapper(menu1.main)
