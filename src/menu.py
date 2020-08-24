4e9adb144a4e8ab46e06a6dad44b39a642fe6be6
James Eapen <jamespeapen@gmail.com>
2020-08-24 13:18:07 -0400

    basic food and recipie preview; window sizes are half

--- src/menu.py
+++ src/menu.py
@@ -17,6 +17,7 @@ class Menu:
         # kitchen and data objects
         self.data = Data()
         self.kitchen = Kitchen()
+        self.foods = self.data.read_food('test/test_read_food.json')
         self.kitchen.pantry = self.data.read_pantry('test/test_read_pantry.json')
         self.kitchen.recipies = self.data.read_recipies('test/test_read_recipies.json')
 
@@ -27,12 +28,12 @@ class Menu:
         # windows
         self.menubar = curses.newwin(2, self.width, 0, 0)
 
-        self.data_window = curses.newwin(self.height-3,
-                                         2 * (self.width // 3), 3, 0)
+        self.data_window = curses.newwin(self.height - 3,
+                                         (self.width // 2), 3, 0)
 
         self.preview_window = curses.newwin(self.height - 3,
-                                            self.width // 3, 4,
-                                            2 * (self.width // 3))
+                                            self.width // 2, 3,
+                                            (self.width // 2))
 
         # colors
         curses.start_color()
@@ -56,9 +57,11 @@ class Menu:
         self.menubar.refresh()
 
     def pantry(self, stdscr, current_row):
-        """Show pantry in the data window"""
+        """Show pantry in the data window and food details
+        in the preview window"""
 
         self.data_window.clear()
+        self.preview_window.clear()
 
         self.data_window.addstr(0, 0, "Food\tQuantity in stock")
         y = 2
@@ -69,18 +72,24 @@ class Menu:
                 self.data_window.attron(curses.color_pair(1))
                 self.data_window.addstr(y, x, food + '\t' + str(self.kitchen.pantry[food]))
                 self.data_window.attroff(curses.color_pair(1))
+                self.preview_window.addstr(str(self.kitchen.pantry[food]))
             else:
                 self.data_window.addstr(y, x, food + '\t' + str(self.kitchen.pantry[food]))
 
             y += 2
 
         self.data_window.refresh()
+        self.preview_window.refresh()
 
     def recipies(self, stdscr, current_row):
-        """show recipies in the data window"""
+        """show recipies in the data window and details
+        in preview window"""
 
         self.data_window.clear()
+        self.preview_window.clear()
+
         self.data_window.addstr(0, 0, "Recipies")
+        self.preview_window.addstr(0, 0, "Ingredient\tQuantity\n\n")
         y = 2
         x = 0
 
@@ -89,6 +98,8 @@ class Menu:
                 self.data_window.attron(curses.color_pair(1))
                 self.data_window.addstr(y, x, recipie.name)
                 self.data_window.attroff(curses.color_pair(1))
+                for ingredient in recipie.ingredients:
+                    self.preview_window.addstr(ingredient + '\t' + str(recipie.ingredients[ingredient]) + '\n\n')
 
             else:
                 self.data_window.addstr(y, x, recipie.name)
@@ -96,6 +107,7 @@ class Menu:
             y += 2
 
         self.data_window.refresh()
+        self.preview_window.refresh()
 
     def refresh_data_window(self, stdscr, current_row, menu_mode):
         """Check what menu item method was selected before scroll event
