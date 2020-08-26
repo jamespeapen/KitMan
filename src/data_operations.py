7c25689c22e9ccd0bf37a2f22e4127cb1765dc85
James Eapen <jamespeapen@gmail.com>
2020-08-26 14:36:12 -0400

    updates for commit 11767f8 - pantry operations

--- src/data_operations.py
+++ src/data_operations.py
@@ -11,7 +11,7 @@ import json
 
 class Data:
 
-    def read_food(self, filename):
+    def read_pantry(self, filename):
         '''
         Read food objects from json file and make
         food objects from them
@@ -26,13 +26,17 @@ class Data:
                 foods.append(food_obj)
         return foods
 
-    def write_food(self, foods, filename):
+    def write_pantry(self, pantry, filename):
         '''
         write a list of foods to a json file
         :param foods: list of food objects
         :param filename: json file to write food objects to
         '''
         with open(filename, 'w') as file:
+
+            foods = []
+            for food in pantry:
+                foods.append(pantry[food])
 
             file.write(json.dumps([food.__dict__ for food in foods], indent=4))
 
@@ -62,37 +66,6 @@ class Data:
 
             file.write(json.dumps(
                 [recipie.__dict__ for recipie in recipies], indent=4))
-
-    def read_pantry(self, filename):
-        '''
-        reads pantry contents from file. only the names and quantity
-        are written to disk
-        @recipies: list of recipie objects
-        :param filename: the json file containing pantry data
-        '''
-
-        with open(filename, 'r') as pantry_file_reader:
-            pantry = json.loads(pantry_file_reader.read())
-        return pantry
-
-    def write_pantry(self, pantry, filename):
-        '''
-        writes the names and quantity of food objects in the pantry
-        to file
-        :param pantry: dictionary of foods and quantities from a
-        Kitchen object
-        :param filename: json file to write pantry to
-        '''
-
-        writable_pantry = {}
-
-        # extract names and quantities
-        for food in pantry:
-            writable_pantry[food.name] = pantry[food]
-
-        with open(filename, 'w') as pantry_file_writer:
-            pantry_file_writer.write(json.dumps(writable_pantry, indent=4))
-        pantry_file_writer.close()
 
     def write_shopping_list(self, shopping_list, filename):
         '''
