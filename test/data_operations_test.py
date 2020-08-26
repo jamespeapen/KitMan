82bac506243eac6430875f0226e1ebd960584672
James Eapen <jamespeapen@gmail.com>
2020-08-26 14:38:19 -0400

    test updates for 11767f8 - pantry operations

--- test/data_operations_test.py
+++ test/data_operations_test.py
@@ -6,23 +6,22 @@ class TestData:
     '''Tests the data module'''
 
 
-    def test_write_food(self):
+    def test_write_pantry(self):
 
         # initialize kitchen with food and recipies
         kitchen = Kitchen()
 
         food1 = Food(name='food1', unit='unit1', category='category1', quantity_needed_in_stock=10)
         food2 = Food('food2', 'category2', 'unit2', quantity_needed_in_stock=20)
-        foods = [food1, food2]
 
         kitchen.add_to_pantry(food1, 10)
         kitchen.add_to_pantry(food2, 20)
 
         data = Data()
-        data.write_food(foods, 'test/test_write_food.json')
+        data.write_pantry(kitchen.pantry, 'test/test_write_pantry.json')
 
         # check file for valid json objects
-        with open('test/test_write_food.json') as file:
+        with open('test/test_write_pantry.json') as file:
             json_data = json.loads(file.read())
 
             assert type(json_data[0]) == dict
@@ -30,22 +29,24 @@ class TestData:
             assert json_data[0]['_unit'] == 'unit1'
             assert json_data[0]['_category'] == 'category1'
             assert json_data[0]['_quantity_needed_in_stock'] == 10
+            assert json_data[0]['_quantity_in_stock'] == 10
 
             assert type(json_data[1]) == dict
             assert json_data[1]['_name'] == 'food2'
             assert json_data[1]['_unit'] == 'unit2'
             assert json_data[1]['_category'] == 'category2'
             assert json_data[1]['_quantity_needed_in_stock'] == 20
+            assert json_data[1]['_quantity_in_stock'] == 20
 
         file.close()
 
         # clear file after test
-        with open('test/test_write_food.json', 'w') as file:
-              file.write('')
+        #with open('test/test_write_pantry.json', 'w') as file:
+              #file.write('')
 
-    def test_read_food(self):
+    def test_read_pantry(self):
         data = Data()
-        foods = data.read_food('test/test_read_food.json')
+        foods = data.read_pantry('test/test_read_food.json')
 
         assert len(foods) == 2
 
@@ -133,57 +134,13 @@ class TestData:
               file.write('')
         file.close()
 
-    def test_read_pantry(self):
-        data = Data()
-
-        pantry = data.read_pantry('test/test_read_pantry.json')
-
-        assert len(pantry) == 4
-        assert pantry['food1'] == 1
-        assert pantry['food2'] == 2
-        assert pantry['food3'] == 3
-        assert pantry['food4'] == 4
-
-    def test_write_pantry(self):
-
-        kitchen = Kitchen()
-
-        food1 = Food(name='food1', unit='unit1', category='category1', quantity_needed_in_stock=10)
-        food2 = Food('food2', 'category2', 'unit2', quantity_needed_in_stock=20)
-        food3 = Food('food3', 'category3', 'unit3', quantity_needed_in_stock=30)
-        food4 = Food('food4', 'category4', 'unit4', quantity_needed_in_stock=40)
-
-        foods = [food1, food2]
-
-        kitchen.add_to_pantry(food1, 10)
-        kitchen.add_to_pantry(food2, 20)
-        kitchen.add_to_pantry(food3, 30)
-        kitchen.add_to_pantry(food4, 40)
-
-        data = Data()
-        data.write_pantry(kitchen.pantry, 'test/test_write_pantry.json')
-
-        with open('test/test_write_pantry.json', 'r') as written_pantry:
-            pantry_dict = json.loads(written_pantry.read())
-
-            assert pantry_dict['food1'] == 10
-            assert pantry_dict['food2'] == 20
-            assert pantry_dict['food3'] == 30
-            assert pantry_dict['food4'] == 40
-        written_pantry.close
-
-        #clear file
-        with open('test/test_write_pantry.json', 'w') as written_pantry:
-            written_pantry.write('')
-        written_pantry.close()
-
     def test_write_shopping_list(sefl):
 
         kitchen1 = Kitchen()
         assert len(kitchen1.pantry) == 0
 
         # add food to pantry
-        food1 = Food('food1', 'category1', 'unit1')
+        food1 = Food('food1', 'category1', 'unit1', quantity_needed_in_stock=2)
         food2 = Food('food2', 'category2', 'unit2', quantity_needed_in_stock=4)
         food3 = Food('food3', 'category3', 'unit3', quantity_needed_in_stock=10)
 
